5cc3d46aefa0be5f46001f9e9d47f40271490360
Zubair Chowdhury <zubairchowdhury12@hotmail.co.uk>
2017-08-04 00:07:03 +0100

    Implement populating time array with calendar events.

---
+++ app/src/main/java/plan/glo/windowplanner/models/ScheduleException.java
@@ -0,0 +1,8 @@
+package plan.glo.windowplanner.models;
+
+/**
+ * Created by zubairchowdhury on 03/08/2017.
+ */
+
+public class Schedule extends RuntimeException {
+}
--- app/src/main/java/plan/glo/windowplanner/models/Scheduler.java
+++ app/src/main/java/plan/glo/windowplanner/models/Scheduler.java
@@ -1,5 +1,9 @@
 package plan.glo.windowplanner.models;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -7,13 +11,64 @@ import java.util.List;
  */
 
 public class Scheduler implements SchedulerI {
-    private CalendarI calendar;
-    private List<Task> tasks;
     private int[] timeArray;
 
     @Override
     public CalendarI schedule(CalendarI calendarI, List<TaskI> tasks) {
+        Date endTime = findFurthestEndTime(tasks);
+        Date scheduleStartTime = new Date(System.currentTimeMillis());
+        int blocksUntilEndTime = blocksInInterval(scheduleStartTime, endTime);
+        timeArray = new int[blocksUntilEndTime];
+
+        List<EventI> calendarEvents = calendarI.getEvents();
+        for (EventI e : calendarEvents) {
+            Date eventStart = e.getEventStartTime();
+            Date eventEnd = e.getEventEndTime();
+            int blocksInEvent = blocksInInterval(eventStart, eventEnd);
+            int startIndex = blocksInInterval(scheduleStartTime, eventStart);
+            int endIndex = startIndex + blocksInEvent;
+            fillInBlock(startIndex, endIndex, e);
+        }
+
         return null;
+    }
+
+    private void fillInBlock(int startIdx, int endIdx, EventI e) {
+        for (int i = startIdx; i <= endIdx; i++) {
+           timeArray[i] = e.getTaskId();
+        }
+    }
+
+    private int blocksInInterval(Date startTime, Date endTime) {
+        if (endTime.after(startTime)) {
+            throw new ScheduleException();
+        }
+
+        long endTimeMs = endTime.getTime();
+        long startTimeMs = startTime.getTime();
+        long difference = endTimeMs - startTimeMs;
+
+        long hours = difference / 1000 / 60 / 60;
+
+        long numberOfBlocks = hours * 2;
+
+        return (int) numberOfBlocks;
+
+    }
+
+    private Date findFurthestEndTime(List<TaskI> tasks) {
+        if (tasks.isEmpty()) throw new ScheduleException();
+        Date maxTime = tasks.get(0).getTaskEndDate();
+
+        for (TaskI t : tasks) {
+
+            Date currEndTime = t.getTaskEndDate();
+            if (currEndTime.compareTo(maxTime) == 1) {
+                maxTime = currEndTime;
+            }
+        }
+
+        return maxTime;
     }
 
     @Override
