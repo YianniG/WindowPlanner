8b7d044829ff378302c44459b2f8c5493f6b61b7
Zubair Chowdhury <zubairchowdhury12@hotmail.co.uk>
2017-08-07 22:55:18 +0100

    Implement Task class

--- app/src/main/java/plan/glo/windowplanner/models/Task.java
+++ app/src/main/java/plan/glo/windowplanner/models/Task.java
@@ -4,38 +4,52 @@ import java.util.List;
 
 public class Task implements TaskI{
 
-   private List<Job> jobs;
+   private List<JobI> jobs;
    private Date startDate;
    private Date endDate;
    private String name;
+   private int id;
 
    @Override
    public int getId() {
-      return 0;
+      return id;
    }
 
    @Override
    public String getTaskName() {
-      return null;
+      return name;
    }
 
    @Override
    public Date getTaskEndDate() {
-      return null;
+      return endDate;
    }
 
    @Override
    public Date getTaskStartDate() {
-      return null;
+      return startDate;
    }
 
    @Override
    public List<JobI> getJobs() {
-      return null;
+      return this.jobs;
    }
 
    @Override
    public void modifyTask(Task configObject) {
+      this.jobs = configObject.getJobs() == null ? this.getJobs() : configObject.getJobs();
+      this.startDate = configObject.getTaskStartDate() == null ? this.getTaskStartDate() : configObject.getTaskStartDate();
+      this.endDate = configObject.getTaskEndDate() == null ? this.getTaskEndDate() : configObject.getTaskEndDate();
+      this.name = configObject.getTaskName() == null ? this.getTaskName(): configObject.getTaskName();
+   }
 
+   @Override
+   public JobI getFreeJob() {
+      for (JobI j : this.jobs) {
+         if (!j.hasEvent()) {
+            return j;
+         }
+      }
+      return null;
    }
 }
