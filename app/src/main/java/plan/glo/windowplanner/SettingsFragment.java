600239073df8eae9c67333add040b02798ccdc33
adeboyed <david.adeboye@outlook.com>
2017-08-08 22:07:29 +0100

    Create general structure of the application

---
+++ app/src/main/java/plan/glo/windowplanner/SettingsFragment.java
@@ -0,0 +1,14 @@
+package plan.glo.windowplanner;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
--- app/src/main/java/plan/glo/windowplanner/TaskFormActivity.java
+++ app/src/main/java/plan/glo/windowplanner/TaskFormActivity.java
@@ -1,10 +1,32 @@
 package plan.glo.windowplanner;
 
+import android.app.DatePickerDialog;
+import android.graphics.PorterDuff;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by zubairchowdhury on 02/08/2017.
  */
 
-public class TaskFormActivity extends AppCompatActivity {
+public class TaskFormActivity extends PreferenceActivity {
+
+
 }
