7a8becd8a2e20ebbb74c31bebb8f568f47887fb7
adeboyed <david.adeboye@outlook.com>
2017-08-21 23:33:05 +0100

    Add new fragments for the MainActivity views

---
+++ app/src/main/java/plan/glo/windowplanner/DayViewFragment.java
@@ -0,0 +1,13 @@
+package plan.glo.windowplanner;
+
+import android.support.v4.app.Fragment;
+
+public class DayViewFragment extends Fragment {
+
+    public static DayViewFragment newInstance(){
+        DayViewFragment frag1 = new DayViewFragment();
+        return frag1;
+    }
+
+
+}
---
+++ app/src/main/java/plan/glo/windowplanner/TaskViewFragment.java
@@ -0,0 +1,14 @@
+package plan.glo.windowplanner;
+
+
+import android.support.v4.app.Fragment;
+
+public class TaskViewFragment extends Fragment{
+
+    public static TaskViewFragment newInstance(){
+        TaskViewFragment frag1 = new TaskViewFragment();
+        return frag1;
+    }
+
+
+}
